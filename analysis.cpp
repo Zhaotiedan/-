536fa22827efebc859baf6b116a66af8ee4f020c
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2023-03-26 23:07:39 +0800

    Create analysis.cpp

--- analysis.cpp
+++ analysis.cpp
@@ -1,24 +1,53 @@
 
 #include<iostream>
+#include<vector>
+#include<string>
 #include <mysql.h> // mysql文件
 using namespace std;
-#define SQL_MAX 256		// sql语句字符数组最大值
+#define SQL_MAX 256	// sql语句字符数组最大值
 
-//数据包头部结构体
-struct IP
+string change(string infor)//16进制字符串转2进制字符串
 {
-	string version;//版本协议
-	int headLength;//报头长度
-	int totalLength;//总长度
-	int identiNum;//标识符
-	int signNum;//标志位
-	int offNum;//偏移值
-	int alive;//生存期
-	string agreement;//协议
-	int headChecSum;//报头校验和
-	string desAddr;//源ip地址
-	string sourAddr;//目的ip地址
+	string res;
+	for (int i = 0; i < infor.size(); i++)
+	{
+		
+	}
+}
+//包结构体
+class Packet
+{
+public:
+	int _num;//包编号
+	string _date;//存入时间
+	string _infor;//包信息
 };
+//数据包头部结构体
+class IP
+{
+public:
+	void ipAnalysis(string ipstr)
+	{
+		//版本
+		string temp= ipstr.substr(0, 1);
+		//转成二进制字符串
+		//int vnum = atoi(temp);
+		
+	}
+private:
+	string _version;//版本协议
+	int _headLength;//报头长度
+	int _totalLength;//总长度
+	int _identiNum;//标识符
+	int _signNum;//标志位
+	int _offNum;//偏移值
+	int _alive;//生存期
+	string _agreement;//协议
+	int _headChecSum;//报头校验和
+	string _desAddr;//源ip地址
+	string _sourAddr;//目的ip地址
+};
+
 
 bool connectDB(MYSQL& mysql)
 {
@@ -31,29 +60,29 @@ bool connectDB(MYSQL& mysql)
 	// 3.连接数据库										// 账号	  密码         数据库名
 	MYSQL* ret = mysql_real_connect(&mysql, "127.0.0.1", "root", "zss060310", "packet", 3306, NULL, 0);
 	if (ret == NULL) {
-		printf("数据库连接失败！失败原因：%s\n", mysql_error(&mysql));
+		cout << "connect error:" << mysql_error(&mysql);
 		return false;
 	}
+	cout << "connect success." << endl;
 
-	printf("数据连接成功！\n");
-
-	// 选择数据库，成功返回0，失败返回非0
+	// 选择数据库
 	int res = mysql_select_db(&mysql, "packet");
-	if (res) {
-		printf("选择数据库失败！失败原因%s\n", mysql_error(&mysql));
+	if (res) 
+	{
+		cout << "choose error:" << mysql_error(&mysql) << endl;
 		return false;
 	}
-	printf("数据库选择成功！\n");
-
+	cout << "choose success." << endl;
 	return true;
 }
 
-bool queTableData() 
+
+bool queTableData(vector<Packet>& vp)
 {
 	MYSQL mysql;		// 数据库句柄
 	MYSQL_RES* res;		// 查询结果集
 	MYSQL_ROW row;		// 记录结构体
-	char sql[SQL_MAX];	// SQL语句
+	char sql[SQL_MAX];	// 保存SQL语句
 
 	// 连接数据库
 	if (!connectDB(mysql)) {
@@ -62,44 +91,52 @@ bool queTableData()
 
 	// C语言组合字符串
 	snprintf(sql, SQL_MAX, "SELECT id, time, packet FROM packet;");
-	printf("查询sql语句：%s\n", sql);
+	cout << "sql:" << sql << endl;
+	//printf("查询sql语句：%s\n", sql);
 
-	// 查询数据
-	//int ret = mysql_query(&mysql, "select * from student;");		// 等效于下面一行代码
-	//int ret = mysql_query(&mysql, sql);
+	// 查询
 	int ret = mysql_real_query(&mysql, sql, (unsigned long)strlen(sql));
-	printf("执行查询语句，查询返回结果：%d\n", ret);
 
 	if (ret) {
-		printf("数据查询失败！失败原因：%s\n", mysql_error(&mysql));
+		cout << "select error:" << mysql_error(&mysql) << endl;
 		return false;
 	}
-	printf("数据查询成功！\n");
-
+	cout << "select success." << endl;
 
 	// 获取结果集
 	res = mysql_store_result(&mysql);
 
-	// 获取查询到的一行数据
-	// 给row赋值，判断row是否为空，不为空就打印数据。
+	// 获取数据,将结果插入vp数组，并打印
 	while (row = mysql_fetch_row(res)) 
 	{
-		cout << atoi(row[0])<<"  " << row[1] << "  "<<row[2] << endl;
+		Packet p;
+		p._num = atoi(row[0]);
+		p._date = row[1];
+		p._infor = row[2];
+		vp.push_back(p);
+		cout << p._num << " " << p._date << " " << p._infor << " " << endl;
 		cout << endl;
 	}
-
 	// 释放结果集
 	mysql_free_result(res);
-
 	// 关闭数据库
 	mysql_close(&mysql);
-
 	return true;
 }
 
+
+void Analysis(vector<Packet> vp)
+{
+	//ip:从112+长度个字节开始存储
+
+}
 int main()
 {
-	//1.连接数据库
-	queTableData();
+	//1.连接数据库，将结果保存到packet对象数组
+	vector<Packet> vp;
+	queTableData(vp);
+
+	//2.协议分析
+	Analysis(vp);
 	return 0;
 }
